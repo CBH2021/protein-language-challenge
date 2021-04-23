ac85af47075f12c392e64fe26f19e9b68ca38bd8
Eryk96 <erik.kiehl@hotmail.dk>
2021-04-23 13:50:51 +0000

    Updated config

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir data
-wget "https://www.dropbox.com/s/xbrnv1mpl4sjg26/CASP12_ESM1b.npz?dl=0" -O CASP12_ESM1b.npz
-mv CASP12_ESM1b.npz data/CASP12_ESM1b.npz
+wget "https://www.dropbox.com/s/wbf1ez145i4dtwn/TS115_ESM1b.npz?dl=0" -O TS115_ESM1b.npz
+mv TS115_ESM1b.npz data/TS115_ESM1b.npz
 
 wget "https://www.dropbox.com/s/dpar81d79ms1sog/Train_ESM1b.npz?dl=0" -O Train_ESM1b.npz
 mv Train_ESM1b.npz data/Train_ESM1b.npz
