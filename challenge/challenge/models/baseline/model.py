dcf579dd43193a289e9b6e9277a6930dde93a881
adzcodez <adamdeharo@gmail.com>
2021-04-24 12:25:17 +0100

    update model

--- challenge/challenge/models/baseline/model.py
+++ challenge/challenge/models/baseline/model.py
@@ -44,7 +44,7 @@ class NetSurfModel(ModelBase):
 
         self.cnn1 = nn.Conv1d(in_channels=in_features, out_channels=(in_features/32), kernel_size=32, stride=1, padding=0) # 1280/32 = 40 out features
         self.cnn2 = nn.Conv1d(in_channels=in_features, out_channels=(in_features/32), kernel_size=32, stride=1, padding=0)
-        self.bilstm = nn.LSTM(input_size=in_features/16, hidden_size=1024, num_layers=2, bidirectional=True, dropout=dropout, output=)
+        self.bilstm = nn.LSTM(input_size=in_features/16, hidden_size=1024, num_layers=2, bidirectional=True, dropout=dropout)
         self.dropout = nn.Dropout(dropout)
 
         # self.ss8 = nn.Linear(in_features=in_features, out_features=8)
